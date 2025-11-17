907a8d3249c5e10c603e623415f24a5ecbd773cd
viralkansodiya <viral@fosserp.com>
2025-11-17 08:28:21 +0530

    fix: create a travel request from travel planning

--- harro/harro/docevents/travel_planning.py
+++ harro/harro/docevents/travel_planning.py
@@ -14,7 +14,7 @@ def create_travel_plan(names):
 	for row in travel_request:
 		tr_doc = frappe.get_doc("Travel Request", row)
 		if tr_doc.docstatus < 1:
-			frappe.throw("Travel Request should be submitted.<br><ul><li>{0}</li></ul>".format(get_link_to_form("Travel Request", row)))
+			frappe.throw("Travel Request should be submitted.<br><ul><li>{0}</li></ul>".format(get_link_to_form('Travel Request', row)))
 		if len(tr_doc.itinerary):
 			for tr in tr_doc.itinerary:
 				travel_planing.append("travel_itinerary", {
@@ -39,6 +39,6 @@ def create_travel_plan(names):
 	message = """ <p>Travel Planning is Created.</p> """
 	message += "<ul>"
 	for tp in travel_planing_list:
-		message += f"<li>{get_link_to_form("Travel Planning", tp)}</li>"
+		message += f"<li>{get_link_to_form('Travel Planning', tp)}</li>"
 	message += "</ul>"
 	frappe.msgprint(message)

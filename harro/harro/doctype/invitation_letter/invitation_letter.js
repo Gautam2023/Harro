edbb5df5292dc00c6b33e5beef9c0830319b6893
viralkansodiya <viral@fosserp.com>
2025-11-17 21:53:35 +0530

    fix: doctype push to production

---
+++ harro/harro/doctype/invitation_letter/invitation_letter.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Fosserp and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Invitation Letter", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ harro/harro/doctype/invitation_letter/test_invitation_letter.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Fosserp and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestInvitationLetter(FrappeTestCase):
+	pass

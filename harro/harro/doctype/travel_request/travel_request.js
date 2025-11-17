aa2c6de6d702dd403d7ada51d888e2969750aeaf
viralkansodiya <viral@fosserp.com>
2025-11-17 20:52:13 +0530

    fix: label

--- harro/harro/doctype/travel_request/travel_request.js
+++ harro/harro/doctype/travel_request/travel_request.js
@@ -4,7 +4,7 @@
 frappe.ui.form.on("Travel Request", {
 	refresh(frm) {
         if(frm.doc.docstatus == 1){
-            frm.add_custom_button(__("Travel Request"), function () {
+            frm.add_custom_button(__("Travel Planning"), function () {
                 frappe.call({
                     method : "harro.harro.docevents.travel_planning.create_travel_plan",
                     args : {

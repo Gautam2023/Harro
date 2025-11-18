05b3ed8bdfc69b33bb8469074190383d85e8563f
viralkansodiya <viral@fosserp.com>
2025-11-18 13:21:37 +0530

    fix: enable the job card timesheet changes

--- harro/public/js/job_card.js
+++ harro/public/js/job_card.js
@@ -13,32 +13,32 @@ frappe.ui.form.on("Job Card", {
                 status: status,
             };
         frm.events.make_time_log(frm, args);
-        // if (status == "Resume Job"){
-        //     frappe.call({
-        //         method: "harro.harro.docevents.job_card.resume_unproductive_log",
-        //         args : {
-        //             to_time : frappe.datetime.now_datetime(),
-        //             job_card : frm.doc.name
-        //         },
-        //         callback:(r)=>{
-        //              const args = {
-        //                 job_card_id: frm.doc.name,
-        //                 start_time: frappe.datetime.now_datetime(),
-        //                 employees: employee,
-        //                 status: status,
-        //             };
-        //             frm.events.make_time_log(frm, args);
-        //         }
-        //     })
-        // }else{
-        //     const args = {
-        //         job_card_id: frm.doc.name,
-        //         start_time: frappe.datetime.now_datetime(),
-        //         employees: employee,
-        //         status: status,
-        //     };
-        //     frm.events.make_time_log(frm, args);
-        // }
+        if (status == "Resume Job"){
+            frappe.call({
+                method: "harro.harro.docevents.job_card.resume_unproductive_log",
+                args : {
+                    to_time : frappe.datetime.now_datetime(),
+                    job_card : frm.doc.name
+                },
+                callback:(r)=>{
+                     const args = {
+                        job_card_id: frm.doc.name,
+                        start_time: frappe.datetime.now_datetime(),
+                        employees: employee,
+                        status: status,
+                    };
+                    frm.events.make_time_log(frm, args);
+                }
+            })
+        }else{
+            const args = {
+                job_card_id: frm.doc.name,
+                start_time: frappe.datetime.now_datetime(),
+                employees: employee,
+                status: status,
+            };
+            frm.events.make_time_log(frm, args);
+        }
 	},
     complete_job: function (frm, status, completed_qty) {
         const args = {
@@ -48,82 +48,82 @@ frappe.ui.form.on("Job Card", {
                 completed_qty: completed_qty,
             };
         frm.events.make_time_log(frm, args);
-        // if(status == 'On Hold'){
-        //     let d = new frappe.ui.Dialog({
-        //     title: 'Update Unproductive Activity',
-        //     fields: [
-        //         {
-        //             "fieldname" : "activity_type",
-        //             "label" : "Activity Type",
-        //             "options" : "Activity Type",
-        //             "reqd" :  1,
-        //             "fieldtype" : "Link"
-        //         },
-        //         {
-        //             "fieldname" : "project",
-        //             "label" : "BA Number",
-        //             "options" : "Project",
-        //             "reqd" :  1,
-        //             "fieldtype" : "Link",
-        //             "default" : frm.doc.project
-        //         },
-        //         {
-        //             "fieldname" : "task",
-        //             "label" : "Task",
-        //             "options" : "Task",
-        //             "reqd" :  0,
-        //             "fieldtype" : "Link"
-        //         },
-        //         {
-        //             "fieldname" : "expected_hrs",
-        //             "label" : "Expected Hrs",
-        //             "reqd" :  0,
-        //             "fieldtype" : "Float"
-        //         }
-        //     ],
-        //     size: 'small', // small, large, extra-large 
-        //     primary_action_label: 'Update Time Log',
-        //     primary_action(values) {
-        //         let data = d.get_values();
-        //         let arg = {
-        //             activity_type : data.activity_type,
-        //             from_time : frappe.datetime.now_datetime(),
-        //             project : data.project,
-        //             task : data.task
-        //         }
-        //         frappe.call({
-        //             method: "harro.harro.docevents.job_card.update_unproductive_log",
-        //             args : {
-        //                 arg : arg,
-        //                 job_card : frm.doc.name
-        //             },
-        //             callback:(r)=>{
-        //                 frm.refresh_field("custom_unproductive_work_timelogs")
-        //                 const args = {
-        //                     job_card_id: frm.doc.name,
-        //                     complete_time: frappe.datetime.now_datetime(),
-        //                     status: status,
-        //                     completed_qty: completed_qty,
-        //                 };
-        //                 frm.events.make_time_log(frm, args);
-        //                 d.hide();
-        //             }
-        //         })
+        if(status == 'On Hold'){
+            let d = new frappe.ui.Dialog({
+            title: 'Update Unproductive Activity',
+            fields: [
+                {
+                    "fieldname" : "activity_type",
+                    "label" : "Activity Type",
+                    "options" : "Activity Type",
+                    "reqd" :  1,
+                    "fieldtype" : "Link"
+                },
+                {
+                    "fieldname" : "project",
+                    "label" : "BA Number",
+                    "options" : "Project",
+                    "reqd" :  1,
+                    "fieldtype" : "Link",
+                    "default" : frm.doc.project
+                },
+                {
+                    "fieldname" : "task",
+                    "label" : "Task",
+                    "options" : "Task",
+                    "reqd" :  0,
+                    "fieldtype" : "Link"
+                },
+                {
+                    "fieldname" : "expected_hrs",
+                    "label" : "Expected Hrs",
+                    "reqd" :  0,
+                    "fieldtype" : "Float"
+                }
+            ],
+            size: 'small', // small, large, extra-large 
+            primary_action_label: 'Update Time Log',
+            primary_action(values) {
+                let data = d.get_values();
+                let arg = {
+                    activity_type : data.activity_type,
+                    from_time : frappe.datetime.now_datetime(),
+                    project : data.project,
+                    task : data.task
+                }
+                frappe.call({
+                    method: "harro.harro.docevents.job_card.update_unproductive_log",
+                    args : {
+                        arg : arg,
+                        job_card : frm.doc.name
+                    },
+                    callback:(r)=>{
+                        frm.refresh_field("custom_unproductive_work_timelogs")
+                        const args = {
+                            job_card_id: frm.doc.name,
+                            complete_time: frappe.datetime.now_datetime(),
+                            status: status,
+                            completed_qty: completed_qty,
+                        };
+                        frm.events.make_time_log(frm, args);
+                        d.hide();
+                    }
+                })
 
                 
-        //     }
-        // });
-        // d.show();
-        // d.set_value("project", frm.doc.project)
+            }
+        });
+        d.show();
+        d.set_value("project", frm.doc.project)
 
-        // }else{
-        //     const args = {
-        //         job_card_id: frm.doc.name,
-        //         complete_time: frappe.datetime.now_datetime(),
-        //         status: status,
-        //         completed_qty: completed_qty,
-        //     };
-        //     frm.events.make_time_log(frm, args);
-        // }
+        }else{
+            const args = {
+                job_card_id: frm.doc.name,
+                complete_time: frappe.datetime.now_datetime(),
+                status: status,
+                completed_qty: completed_qty,
+            };
+            frm.events.make_time_log(frm, args);
+        }
 	},
 })

74288b4b25f9221456c4b163cdc8852bc30e0865
viralkansodiya <viral@fosserp.com>
2025-12-06 13:57:03 +0530

    show the number of item in Material request

--- harro/hooks.py
+++ harro/hooks.py
@@ -57,7 +57,8 @@ doctype_js = {
         "Purchase Order" : "public/js/purchase_order.js",
         "Expense Claim" : "public/js/expense_claim.js",
         "Task" : "public/js/task.js",
-        "Production Plan" : "public/js/production_plan.js"
+        "Production Plan" : "public/js/production_plan.js",
+        "Material Request" : "public/js/material_requiest.js"
     }
 doctype_list_js = {"Task" : "public/js/task_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}

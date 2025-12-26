bc6de2ef9e848d013b0e41a2534a7c083175f906
viralkansodiya <viral@fosserp.com>
2025-12-26 15:45:34 +0530

    feat: added a new report with BA number filters

---
+++ harro/harro/report/requested_items_to_order_and_receive_(_harro_)/requested_items_to_order_and_receive_(_harro_).js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Fosserp and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Requested Items to Order and Receive ( Harro )"] = {
+	"filters": [
+
+	]
+};

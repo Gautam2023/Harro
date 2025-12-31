6f2148422d8de32f0eadb840842e342f78068f8e
Gautam <gautam@Basawarajs-MacBook-Air.local>
2025-12-31 21:22:58 +0530

    update task status in child table and created patch to update older task

--- harro/hooks.py
+++ harro/hooks.py
@@ -193,7 +193,8 @@ doc_events = {
         "validate" : "harro.harro.docevents.project.validate"
     },
     "Task" : {
-        "validate" : "harro.harro.docevents.task.validate"
+        "validate" : "harro.harro.docevents.task.validate",
+        "on_update" : "harro.harro.docevents.task.update_parent_task_dependency_status"
     },
     "Material Request": {
         "on_update": "harro.harro.docevents.material_request.on_update",

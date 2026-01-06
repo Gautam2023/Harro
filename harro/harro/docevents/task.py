d3adc3a0f7895f9cb7c515245e324535db7a69d9
viralkansodiya <viral@fosserp.com>
2026-01-06 13:36:42 +0530

    fix: a

--- harro/harro/docevents/task.py
+++ harro/harro/docevents/task.py
@@ -43,8 +43,9 @@ def update_task_details_of_parent_task(self):
                     _assign = []
                 else:
                     _assign = eval(task_doc._assign)
-                if row.custom_user and row.custom_user not in _assign and not check_if_assignment(self):
-                    add_assignment({"doctype": self.doctype, "name": row.task, "assign_to": [row.custom_user]})
+                if row.custom_user and row.custom_user not in _assign:
+                    if not check_if_assignment(self):
+                        add_assignment({"doctype": self.doctype, "name": row.task, "assign_to": [row.custom_user]})
                     share_a_task_access(self)
                     frappe.db.set_value("Task", row.task, "custom_assigned_to_responsible_user", row.custom_user)
                     frappe.db.set_value("Task", row.task, "custom_employee__assign_to_employee_", row.custom_employee)

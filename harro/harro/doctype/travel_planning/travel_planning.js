c3220af40c34af2678b6ee4fa5af528fbc0cc0a8
viralkansodiya <viral@fosserp.com>
2026-01-16 21:28:49 +0530

    allow to create purchase invoice from Travel planning

--- harro/harro/doctype/travel_planning/travel_planning.js
+++ harro/harro/doctype/travel_planning/travel_planning.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2025, Fosserp and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Travel Planning", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Travel Planning", {
+	refresh(frm) {
+        if(!frm.is_new()){
+            frm.add_custom_button(__("Purchase Invoice"), (frm)=>{
+                frappe.model.open_mapped_doc({
+                    method: "harro.harro.doctype.travel_planning.travel_planning.create_purchase_invoice",
+                    frm: cur_frm,
+                });
+            }, __("Create"))
+        }
+	},
+});
--- harro/harro/doctype/travel_planning/travel_planning.py
+++ harro/harro/doctype/travel_planning/travel_planning.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2025, Fosserp and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
 
 
 class TravelPlanning(Document):
 	pass
+
+@frappe.whitelist()
+def create_purchase_invoice(source_name, target_doc=None):
+	doclist = get_mapped_doc(
+		"Travel Planning",
+		source_name,
+		{
+			"Travel Planning": {
+				"doctype": "Purchase Invoice", 
+				"field_map" : {
+					"name" : "travel_planning"
+				}
+			},
+		},
+		target_doc,
+	)
+
+	return doclist

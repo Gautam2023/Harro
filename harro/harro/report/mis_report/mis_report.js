537695fe4887a61f960fbc429976742eca8e6e54
Gautam <gautamx9420@gmail.com>
2026-02-02 12:52:16 +0530

    Created MIS Report and updated bom_creator method to handle .csv file when fial with utf-8 encoding

---
+++ harro/harro/report/mis_report/mis_report.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2026, Fosserp and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["MIS Report"] = {
+    filters: [
+        {
+            fieldname: "ba_number",
+            label: "BA Number",
+            fieldtype: "Link",
+            options: "Project"
+        },
+        {
+            fieldname: "travel_request",
+            label: "Travel Request",
+            fieldtype: "Link",
+            options: "Travel Request"
+        },
+        {
+            fieldname: "travel_plan",
+            label: "Travel Plan",
+            fieldtype: "Link",
+            options: "Travel Planning"
+        },
+        {
+            fieldname: "from_date",
+            label: "From Date",
+            fieldtype: "Date"
+        },
+        {
+            fieldname: "to_date",
+            label: "To Date",
+            fieldtype: "Date"
+        }
+    ]
+};
+

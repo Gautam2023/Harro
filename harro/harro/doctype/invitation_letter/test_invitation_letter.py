92f0ae8571a26f6426bab7fa2e76d019a6d1f002
Gautam <gautamx9420@gmail.com>
2026-02-16 15:06:50 +0530

    deleted and created invitation letter and covering letter doctype

--- harro/harro/doctype/invitation_letter/test_invitation_letter.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Fosserp and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestInvitationLetter(FrappeTestCase):
-	pass

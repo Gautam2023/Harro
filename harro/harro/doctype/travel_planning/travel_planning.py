c5aa1ac3a775832ca91c43afbcf00150201f9654
Gautam <gautamx9420@gmail.com>
2026-03-04 12:53:25 +0530

    travel planning child table attchemnt fieldname updated

--- harro/harro/doctype/travel_planning/travel_planning.py
+++ harro/harro/doctype/travel_planning/travel_planning.py
@@ -34,8 +34,8 @@ def send_attachment_emails(docname):
     attachment_fields = [
         "custom_evisa",
         "custom_travel_insurance",
-        "custom_flight_ticket",
-        "custom_hotel_voucher"
+        "custom_flight_bill",
+        "custom_taxi_bill"
     ]
 
     for row in doc.travel_itinerary:

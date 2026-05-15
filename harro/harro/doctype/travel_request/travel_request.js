43d0c3411521899e5ce2d8fc23b30eb6e37cafd7
gautam-fosserp <gautam@fosserp.com>
2026-05-15 19:35:06 +0530

    updated employee filter logic to include current logged in user

--- harro/harro/doctype/travel_request/travel_request.js
+++ harro/harro/doctype/travel_request/travel_request.js
@@ -20,9 +20,6 @@ frappe.ui.form.on("Travel Request", {
     },
     custom_checkin_date(frm) {
         calculate_nights_parent(frm);
-    },
-    onload: function(frm) {
-        set_employee_filter(frm);
     }
 });
 
@@ -40,6 +37,8 @@ function calculate_nights_parent(frm) {
 }
 
 function set_employee_filter(frm) {
+    if (frm._employee_filter_set) return;
+
     frappe.call({
         method: 'frappe.client.get_value',
         args: {
@@ -48,27 +47,59 @@ function set_employee_filter(frm) {
             fieldname: 'name'
         },
         callback: function(response) {
-            if (response.message) {
-                const logged_in_employee = response.message.name;
+            const logged_in_employee = response && response.message && response.message.name;
 
+            if (!logged_in_employee) {
                 frm.set_query('employee', function() {
-                    return {
-                        filters: {
-                            reports_to: logged_in_employee,
-                            status: 'Active'
-                        }
-                    };
+                    return { filters: { name: ['in', []] } };
                 });
-            } else {
-                frm.set_query('employee', function() {
-                    return {
-                        filters: {
-                            name: '' 
-                        }
-                    };
-                });
-                frappe.msgprint(__('No Employee record found for the logged-in user.'));
+                frm._employee_filter_set = true;
+                setTimeout(() => {
+                    frappe.msgprint({
+                        title: __('Warning'),
+                        message: __('No Employee record found for the logged-in user. You cannot create a Travel Request.'),
+                        indicator: 'orange'
+                    });
+                }, 500);
+                return;
             }
+
+            frappe.call({
+                method: 'frappe.client.get_list',
+                args: {
+                    doctype: 'Employee',
+                    filters: [
+                        ['reports_to', '=', logged_in_employee],
+                        ['status', '=', 'Active']
+                    ],
+                    fieldname: 'name',
+                    limit: 0
+                },
+                callback: function(r) {
+                    if (!r || r.exc) {
+                        frappe.show_alert({
+                            message: __('Failed to load employee list. Please refresh.'),
+                            indicator: 'red'
+                        });
+                        return;
+                    }
+
+                    const direct_reports = (r.message || []).map(e => e.name);
+                    const allowed = [logged_in_employee, ...direct_reports];
+
+                    frm.set_query('employee', function() {
+                        return {
+                            filters: [
+                                ['name', 'in', allowed],
+                                ['status', '=', 'Active']
+                            ]
+                        };
+                    });
+
+                    frm.refresh_field('employee');
+                    frm._employee_filter_set = true;
+                }
+            });
         }
     });
 }

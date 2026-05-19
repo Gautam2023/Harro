42831b2bd5707f8f520e40a6952b21da4a549c63
gautam-fosserp <gautam@fosserp.com>
2026-05-19 15:20:42 +0530

    updated Total Flight Cost calculation in Travel Planning Child table

--- harro/harro/doctype/travel_planning/travel_planning.js
+++ harro/harro/doctype/travel_planning/travel_planning.js
@@ -278,8 +278,9 @@ function calculate_total_flight_cost(frm, cdt, cdn) {
     let return_flight_cost = row.custom_return_flight_cost_as_per_invoice || 0;
     let set_charge = row.custom_seat_charges || 0;
     let baggage_cost = row.baggage_coast || 0;
+    let round_trip_cost = row.custom_round_trip_cost_as_per_invoice || 0;
 
-    let total_flight_cost = onward_flight_cost + return_flight_cost + set_charge + baggage_cost;
+    let total_flight_cost = onward_flight_cost + return_flight_cost + set_charge + baggage_cost + round_trip_cost;
     frappe.model.set_value(cdt, cdn, "custom_total_flight_cost_as_per_invoice", total_flight_cost);
 }
 

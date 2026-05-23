49ca214f594235729bf90104a42ee8d75825b505
gautam-fosserp <gautam@fosserp.com>
2026-05-23 12:05:29 +0530

    improved set_due_date in purchase_invoice.py

--- harro/hooks.py
+++ harro/hooks.py
@@ -241,7 +241,7 @@ doc_events = {
         "before_save": "harro.harro.docevents.travel_planning.calculate_totals"
     },
     "Purchase Invoice": {
-        "after_save": "harro.harro.docevents.purchase_invoice.set_due_date"
+        "before_save": "harro.harro.docevents.purchase_invoice.set_due_date"
     }
 }
 

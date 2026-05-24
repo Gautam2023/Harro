1b52d4a9e5f1c503aba7d649d8a2118f2eceb79e
gautam-fosserp <gautam@fosserp.com>
2026-05-24 09:31:55 +0530

    updated on_update() method of travel_request.py used frappe.db.commit()

--- harro/harro/docevents/travel_request.py
+++ harro/harro/docevents/travel_request.py
@@ -14,23 +14,24 @@ def on_update(doc, method=None):
 
     sync_travel_planning_from_travel_request(doc)
 
-    # Handle workflow status update (optional UI/email logic)
     trip_status = WORKFLOW_TO_TRIP_STATUS.get(doc.workflow_state)
 
     if not trip_status:
         return
 
-    tr_updated = False
-
     for row in doc.itinerary:
         if row.custom_flight_booking_status != trip_status:
-            row.custom_flight_booking_status = trip_status
-            row.custom_hotel_booking_status = trip_status
-            tr_updated = True
+            frappe.db.set_value(
+                "Travel Itinerary",
+                row.name,
+                {
+                    "custom_flight_booking_status": trip_status,
+                    "custom_hotel_booking_status": trip_status,
+                },
+                update_modified=False
+            )
 
-    if tr_updated:
-        doc.flags.ignore_permissions = True
-        doc.save()
+    frappe.db.commit()
 
 
 def sync_travel_planning_from_travel_request(doc, method=None):
